745cffed17ed57fde16dad82d481d48708269f38
Kostyantin Grigor`ev <grigorev.kh.cr@gmail.com>
2019-04-26 10:36:05 +0300

    Clear cached data before logout

--- src/actions/logout.js
+++ src/actions/logout.js
@@ -4,7 +4,11 @@ import { LoginManager } from 'react-native-fbsdk';
 import AsyncStorage from '@react-native-community/async-storage';
 
 export default async function logout(navigation) {
+    await Promise.all([
+        AsyncStorage.removeItem('userToken'),
+        AsyncStorage.removeItem('profile'),
+        AsyncStorage.removeItem('feed'),
+    ]);
     await LoginManager.logOut();
-    await AsyncStorage.removeItem('userToken');
     navigation.navigate('Login');
 }
